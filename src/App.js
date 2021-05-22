92f4e6e460d93db8631f09bb2afe6eb99f2391dc
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-22 14:56:16 -0300

    Empezando monster-rolodex primer paso

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hola Lauti Silva'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'as1w'
+        }
+      ]
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: 'Hola Lautaro' })}>Change Text</button>                   
-        </header>
+        {
+          this.state.monsters.map(monster => (<h1 key={monster.id}> { monster.name } </h1>))
+        }
       </div>
     );
   }
