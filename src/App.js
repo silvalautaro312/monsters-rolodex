7f4c1c5dc20c1f3696c45ad4398c7b3903ef001c
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-22 16:59:54 -0300

    Utilizamos Life Cycle Method 'componentDidMount' + el API json.placeholder/users

--- src/App.js
+++ src/App.js
@@ -7,27 +7,19 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2'
-        },
-        {
-          name: 'Zombie',
-          id: 'as1w'
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
   }
   render() {
     return (
-      <div className="App">
-        {
-          this.state.monsters.map(monster => (<h1 key={monster.id}> { monster.name } </h1>))
+      <div className='App'>
+        {this.state.monsters.map(monster => (<h1 key={monster.id}> {monster.name} </h1>))
         }
       </div>
     );
