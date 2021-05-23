4a1743a4bf95e5c08d04b4654406759475a89768
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-23 20:05:28 -0300

    agrego titulo y ultimos toques de css

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     .then(users => this.setState({monsters: users}));
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({ searchField: e.target.value })
   }
 
@@ -32,6 +32,7 @@ class App extends Component {
       );
     return (
       <div className='App'>
+        <h1> Monster Rolodex </h1>
         <SearchBox
         placeholder='search monsters'
         handleChange={this.handleChange}
