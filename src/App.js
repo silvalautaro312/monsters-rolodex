0007a4402b1b4cd3a389888aec487f23357ecc87
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-23 13:26:16 -0300

    Agrego cartas de monstruos y css con className card-container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -20,10 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-      <CardList name='Lauti'>
-        {this.state.monsters.map(monster => (<h1 key={monster.id}> {monster.name} </h1>))
-        }
-      </CardList>
+      <CardList monsters={this.state.monsters} />      
       </div>      
     );
   }
