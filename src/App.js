3e4af68dd95fb9385d7ca817b6c8bbcce1f3f4e9
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-23 14:11:06 -0300

    agrego en state el campo de busqueda y en input e.target.value

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -20,7 +21,12 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-      <input type='search' placeholder='search monsters'/>
+      <input 
+      type='search'
+      placeholder='search monsters' 
+      onChange={e => 
+        this.setState({ searchField: e.target.value })}
+    />
       <CardList monsters={this.state.monsters} />      
       </div>      
     );
