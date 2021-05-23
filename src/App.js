3cf7d0ab6348a876600c144211e5c00ec42c5a17
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-23 14:25:27 -0300

    agregamos const en el render para monstruos filtrados

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
     .then(users => this.setState({monsters: users}));
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return (
       <div className='App'>
       <input 
@@ -27,7 +31,7 @@ class App extends Component {
       onChange={e => 
         this.setState({ searchField: e.target.value })}
     />
-      <CardList monsters={this.state.monsters} />      
+      <CardList monsters={filteredMonsters} />      
       </div>      
     );
   }
