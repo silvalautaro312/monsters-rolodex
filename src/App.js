5781b518f55cc28179e7329eb913648e77731906
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-23 17:45:26 -0300

    agregamos componente search-box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
@@ -25,13 +26,12 @@ class App extends Component {
       );
     return (
       <div className='App'>
-      <input 
-      type='search'
-      placeholder='search monsters' 
-      onChange={e => 
-        this.setState({ searchField: e.target.value })}
-    />
-      <CardList monsters={filteredMonsters} />      
+        <SearchBox
+        placeholder='search monsters'
+        handleChange={e => 
+          this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />      
       </div>      
     );
   }
